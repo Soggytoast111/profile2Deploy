54f0e6c7a0f2ab1955585a0340225c5fe3fb261f
Jon Curto <joncurto@gmail.com>
2019-08-11 22:09:12 -0400

    deploy?

---
+++ precache-manifest.01f8a555ec0830c5cbb6e6a5072c54f7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5bde19b4e51514d096e8ca14eaa2c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64f4cadb2dfa35c17d69",
+    "url": "/static/css/main.f6716b96.chunk.css"
+  },
+  {
+    "revision": "4b78010f3ec12dbec223",
+    "url": "/static/js/2.175f74e2.chunk.js"
+  },
+  {
+    "revision": "64f4cadb2dfa35c17d69",
+    "url": "/static/js/main.b4f74d7d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "606bda4dc1cd9815b910544cc7d40b87",
+    "url": "/static/media/RPG.606bda4d.gif"
+  },
+  {
+    "revision": "ebf82a0f405a2b2cd74028deff2a8bc8",
+    "url": "/static/media/bg.ebf82a0f.jpg"
+  },
+  {
+    "revision": "73be24b8643a07b485a7bb1230d3f0f6",
+    "url": "/static/media/burger.73be24b8.gif"
+  },
+  {
+    "revision": "5b04d3fb3524286b5b0acd0b3e1232c4",
+    "url": "/static/media/reddit.5b04d3fb.gif"
+  },
+  {
+    "revision": "1399005cfefb1d471a3eb4137fa38c5f",
+    "url": "/static/media/rock.1399005c.gif"
+  },
+  {
+    "revision": "96fd4612f3f4cd51a4187e3074371671",
+    "url": "/static/media/sf.96fd4612.gif"
+  },
+  {
+    "revision": "7e5542986c0d62cbc8f2245f92047d84",
+    "url": "/static/media/trivia.7e554298.gif"
+  },
+  {
+    "revision": "0e8563cfcfd2714d3dad541022d6bfbb",
+    "url": "/static/media/tubtrivia.0e8563cf.gif"
+  }
+]);
